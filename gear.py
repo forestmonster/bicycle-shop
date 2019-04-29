fb63a4f305e8168ad59d254d5049d5cd76107d8b
Forest Monsen <forest.monsen@gmail.com>
2019-04-28 22:21:01 -0700

    Remove rim and tire methods.

--- gear.py
+++ gear.py
@@ -4,38 +4,26 @@
 class Gear(object):
     """Create a Gear."""
 
-    def __init__(self, chainring, cog, rim, tire):
+    def __init__(self, chainring, cog):
         """Construct a Gear.
 
         :chainring: Integer. Number of teeth on the forward (pedaled) gear.
         :cog: Integer. Number of teeth on the rear (wheel) cassette.
-        :rim: Integer. Rim diameter.
-        :tire: Integer. Tire diameter.
 
         """
         self._chainring = chainring
         self._cog = cog
-        self._rim = rim
-        self._tire = tire
         print(
             f"Creating Gear with a chainring of {self._chainring}, a cog of",
-            f"{self._cog}, a rim of {self._rim}, and a tire diameter of",
-            f"{self._tire}.",
+            f"{self._cog}.",
         )
         print(f"Ratio is {self.ratio()}.")
-        print(f"Gear inches is {self.gear_inches()}.")
 
     def ratio(self):
         """Calculate the gear ratio."""
         ratio = self._chainring / self._cog
         return ratio
 
-    def gear_inches(self):
-        """Calculate the gear inches."""
-        gear_inches = self.ratio() * (self._rim + (self._tire * 2))
-        return gear_inches
+gear = Gear(chainring=52, cog=11)
 
-
-gear = Gear(chainring=52, cog=11, rim=26, tire=1.5)
-
-gear = Gear(chainring=30, cog=27, rim=19, tire=1.5)
+gear = Gear(chainring=30, cog=27)
